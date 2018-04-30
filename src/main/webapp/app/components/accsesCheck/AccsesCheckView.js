b7bca09816a61f5e95107e9521c68c1b2048aec9
Aytekin <aytekinerlale@gmail.com>
2018-04-30 05:54:57 +0300

    only access check page editing...

--- src/main/webapp/app/components/accsesCheck/AccsesCheckView.js
+++ src/main/webapp/app/components/accsesCheck/AccsesCheckView.js
@@ -43,41 +43,25 @@ define(['text!components/accsesCheck/AccsesCheckTemplate.html'], function (templ
 
         },
         events: {
-            'submit #cityForm': 'saveCity',
-            'click .deleteCity': 'deleteCity',
+
             'click .editCity': 'openEditMode',
             'click .cancel': 'cancelUpdate',
-            'click .updateCity': 'updateCity'
-        },
-        saveCity: function (e) {
-            e.preventDefault();
+            "change #entryPlaceId": "searchRecord"
 
-            var city = new CityModel({
-                epermit_names:$("#epermit_names").val(),
-                entryDate:$("#entryDate").val(),
-                exitDate:$("#exitDate").val(),
-                enterTime:$("#enterTime").val(),
-                exitTime:$("#exitTime").val(),
-                entryCompany:$("#entryCompany").val(),
-                reasonfentry:$("#reasonfentry").val(),
-                accompanyPersonal:$("#accompanyPersonal").val(),
-                entryPlaceId : $("#entryPlaceId").val(),
-                wsEducation:$("#wsEducation").val()
-            });
+        },
+        searchRecord: function (e) {
+            //select listte bir değişme olduysa
+            if(e.currentTarget.value != ""){
+                //firma ismi seçilmiş ise işlemler gerçekleştirilecek
+                //gelen value ile firma ismi karşılaştırılıp ona göre bilgiler çekilecek
+                //yeni bir model ile olabilir...
+                alert(e.currentTarget.value);
+            }
 
-            this.cities.create(city, {wait: true});
-        },
-        deleteCity: function (e) {
-            var id = $(e.currentTarget).data("id");
-            this.cities.findWhere({id: id}).destroy();
-        },
-        updateCity: function (e) {
-            var row = $(e.currentTarget).closest("tr");
-            var newCityName = row.find("input").val();
-            var id = $(e.currentTarget).data("id");
-            var city = this.cities.findWhere({id: id});
-            city.set({userName: newCityName});
-            city.save();
+            //bu fonksiyon elementleri saklıyor .show() ise gösteriyor
+            this.$el.find(".normalModeElement").hide();
+
+
 
         },
         openEditMode: function (e) {
